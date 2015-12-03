46bed513e05718b608fd4d3d9989e29d0e1a4fd2
duckrxy <duckrxy@hotmail.com>
2015-12-03 23:25:39 +0800

    reorg the angular js app

---
+++ bayesian/app/static/app/scripts/app.js
@@ -0,0 +1,44 @@
+﻿var app = angular.module('ui.bootstrap.demo', ['ui.bootstrap', 'ngAnimate', 'ngTouch']);
+
+app.controller('CarouselDemoCtrl', function ($scope) {
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    var slides = $scope.slides = [];
+    $scope.addSlide = function () {
+        var newWidth = 600 + slides.length + 1;
+        slides.push({
+            //image: '//placekitten.com/' + newWidth + '/300',
+            image: '../../static/app/images/carousel1.jpg',
+            text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' +
+              ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+        });
+    };
+    for (var i = 0; i < 4; i++) {
+        $scope.addSlide();
+    }
+    $scope.t = 'sksksksk';
+});
+
+app.controller('DropdownCtrl', function ($scope, $log) {
+    $scope.items = [
+      'The first choice!',
+      'And another choice for you.',
+      'but wait! A third!'
+    ];
+
+    $scope.status = {
+        isopen: false
+    };
+
+    $scope.toggled = function (open) {
+        $log.log('Dropdown is now: ', open);
+    };
+
+    $scope.toggleDropdown = function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+
+    $scope.isCollapsed = false;
+});
